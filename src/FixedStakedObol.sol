560a89e2a82b34ab2730435e79e86dd8ac85f1d9
Ben DiFrancesco <ben@scopelift.co>
2025-04-14 15:34:22 -0400

    Add natspec to the Obol staking contracts

--- src/FixedStakedObol.sol
+++ src/FixedStakedObol.sol
@@ -6,7 +6,24 @@ import {FixedGovLstPermitAndStake} from "stGOV/extensions/FixedGovLstPermitAndSt
 import {FixedGovLstOnBehalf} from "stGOV/extensions/FixedGovLstOnBehalf.sol";
 import {GovLst} from "stGOV/GovLst.sol";
 
+/// @title FixedStakedObol
+/// @author [ScopeLift](https://scopelift.co)
+/// @notice The fixed balance variant of the liquid staked OBOL token, i.e. stOBOL. This contract
+/// works in tandem with the rebasing staked OBOL token, i.e. rstOBOL. While this contract is the
+/// canonical liquid representation of staked OBOL, the two use the same underlying accounting
+/// system, which is located in the rebasing token contract. This contract is deployed by, and
+/// interacts with, the rstOBOL contract. Unlike rstOBOL, this contract maintains a fixed balance
+/// for stakers, even as rewards are distributed. While the user's balance of stOBOL stays fixed,
+/// the number of underlying OBOL tokens they have a claim to grows over time. As such, 1 stOBOL
+/// will be worth more and more OBOL over time.
 contract FixedStakedObol is FixedGovLst, FixedGovLstPermitAndStake, FixedGovLstOnBehalf {
+  /// @notice Initializes the fixed balance staked OBOL contract.
+  /// @param _name The name for the fixed balance liquid stake token.
+  /// @param _symbol The symbol for the fixed balance liquid stake token.
+  /// @param _lst The rebasing LST for which this contract will serve as the fixed balance
+  /// counterpart.
+  /// @param _stakeToken The ERC20 token that acts as the staking token.
+  /// @param _shareScaleFactor The scale factor applied to shares in the rebasing contract.
   constructor(
     string memory _name,
     string memory _symbol,
