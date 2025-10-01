e2f3479975096cbaf7abab9114aaccda2418708f
keating <keating.dev@protonmail.com>
2025-10-01 16:29:15 -0400

    WIP Script

---
+++ script/MainnetWrappedStakedObol.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import {BaseWrappedStakedObolDeploy} from "script/BaseWrappedStakedObol.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract MainnetWrappedStakedObolDeploy is BaseWrappedStakedObolDeploy {
+  function _getWrapperConfig() internal virtual override returns (ObolWrapperParams memory) {
+    uint256 _prefundAmount = 1e18;
+
+    address _expectedWrappedLstAddress = vm.computeCreateAddress(deployer, vm.getNonce(deployer) + 1);
+	vm.broadcast(deployer);
+	IERC20(0x01Fe882DBB791Aa17BbcBb054214ecd3eB809c39).approve(_expectedWrappedLstAddress, _prefundAmount);
+    return BaseWrappedStakedObolDeploy.ObolWrapperParams({
+      name: "Wrapped Staked Obol",
+      symbol: "wstOBOL",
+      lst: 0x1932e815254c53B3Ecd81CECf252A5AC7f0e8BeA,
+      delegatee: 0xC0c2fC4e158b9F51AE484B6bd66BB7185085C40a,
+      initialOwner: 0x42D201CC4d9C1e31c032397F54caCE2f48C1FA72,
+      preFundAmount: _prefundAmount
+    });
+  }
+}
